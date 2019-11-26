d89d02d749cc6a24e41263916f05bc72f01a7e75
Patryk Płaczek <patryk.placzek@wp.pl>
2019-11-26 19:13:44 +0100

    Add task 18.3

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import {Row, Col} from 'react-flexbox-grid';
-import PropTypes from 'prop-types';
+import { Row, Col } from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import PropTypes from 'prop-types';
+import OrderOption from '../OrderOption/OrderOption';
+import pricing from '../../../data/pricing.json';
 
-
-const OrderForm = props => {
-  const { tripCost, options } = props;
+const OrderForm = ({ tripCost, options, setOrderOption }) => {
 
   return (
     <Row>
+      {pricing.map(option => {
+        return (
+          <Col key={option.id} md={4}>
+            <OrderOption
+              currentValue={options[option.id]}
+              setOrderOption={setOrderOption}
+              {...option}
+            />
+          </Col>
+        );
+      })}
       <Col xs={12}>
         <OrderSummary tripCost={tripCost} options={options} />
       </Col>
@@ -17,8 +28,9 @@ const OrderForm = props => {
 };
 
 OrderForm.propTypes = {
-  tripCost: PropTypes.string,
-  options: PropTypes.object,
+  tripCost: PropTypes.string.isRequired,
+  options: PropTypes.object.isRequired,
+  setOrderOption: PropTypes.func.isRequired,
 };
 
 export default OrderForm;
